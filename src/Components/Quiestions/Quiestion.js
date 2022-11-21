fa771942c16f6f6953332f442b63188be3f22a6e
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-11-21 15:11:18 +0800

    added toast in answer feedback

--- src/Components/Quiestions/Quiestion.js
+++ src/Components/Quiestions/Quiestion.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import SingleQuestion from '../SingleQuestion/SingleQuestion';
 
 const Quiestion = () => {
-    const questions=useLoaderData()
+    const questions = useLoaderData()
+    console.log(questions)
+    const [question, setquestion] = useState([]);
+
+    const eyeHandler = (ques) => {
+        console.log(ques)
+    }
     return (
         <div>
             {
--- src/Components/SingleQuestion/SingleQuestion.js
+++ src/Components/SingleQuestion/SingleQuestion.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
+
 import './Singlequestion.css'
+import swal from 'sweetalert';
 const SingleQuestion = ({ ques }) => {
-    console.log(ques)
-    const { id, question, correctAnswer,options } = ques;
+    const { id, question, correctAnswer, options } = ques;
+    
+    const answerSubmission = (e) => {
+        if (e.target.id === ques.correctAnswer) {
+            console.log('correct')
+            swal("Answer right!", "You clicked the button!", "success");
+        }
+        else {
+            console.log('this answer was wrong')
+            swal("Sorry This Answer False", "You clicked the button!", "error");
+        }
+    }
     return (
         <div >
             <div className='question-main'>
                 <h4>{question}</h4>
+                <EyeIcon className='icons'></EyeIcon>
                 <div className='option-more'>
                 <div className='question-option'>
-                    <p>{ options[0]}</p>
+                        <input onChange={answerSubmission} type="radio" name="questions" id={options[0]} />
+                        <label htmlFor={options[0]}>{ options[0]}</label>
                 </div>
                 <div className='question-option'>
-                    <p>{ options[1]}</p>
+                         <input onChange={answerSubmission} type="radio" name="questions" id={options[1]} />
+                        <label htmlFor={options[1]}>{ options[1]}</label>
                 </div>
                 <div className='question-option'>
-                    <p>{ options[2]}</p>
+                         <input onChange={answerSubmission} type="radio" name="questions" id={options[2]} />
+                        <label htmlFor={options[2]}>{ options[2]}</label>
                 </div>
                 <div className='question-option'>
-                    <p>{ options[3]}</p>
+                        <input onChange={answerSubmission} type="radio" name="questions" id={options[3]} />
+                        <label htmlFor={options[3]}>{ options[3]}</label>
                 </div>
                 </div>
+
             </div>
         </div>
     );
 };
-
+{/* <p>{ options[1]}</p> */}
 export default SingleQuestion;
